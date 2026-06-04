848a4f2da968ea30393715f59a6d115eb9f5a197
dangco <dangcobui09@gmail.com>
2026-06-04 11:10:30 +0700

    update

--- VietLotApi/Program.cs
+++ VietLotApi/Program.cs
@@ -3,23 +3,35 @@ using VietLotApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Server=(localdb)\\mssqllocaldb;Database=VietLotDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
-
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Auto migrate Database on startup (For Docker)
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    try {
+        dbContext.Database.Migrate();
+    } catch(Exception ex) {
+        Console.WriteLine("Migration Error: " + ex.Message);
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+// Configure the HTTP request pipeline.
+// Luôn hiện Swagger kể cả môi trường Production để test dễ dàng
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
